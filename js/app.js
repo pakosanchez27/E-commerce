982daae6ea1dc2af128c696e77194b9abf4558bd
Francisco Sanchez <javier.sanchezjps27@gmail.com>
2022-03-12 05:27:21 -0600

    update: agregando funcionalidad a carrito

--- js/app.js
+++ js/app.js
@@ -1,34 +1,48 @@
 // Variables
 
 const btnMenu = document.querySelector("#menu");
+const btnCarrito = document.querySelector('#carrito');
 const cerrar = document.querySelector("#x");
 const menuMobil = document.querySelector("#menuMobile");
-
+const cardCarrito = document.querySelector('#card-carrito');
 
 // Eventos
 listener();
 function listener() {
   btnMenu.addEventListener("click", mostrarMenu);
-cerrar.addEventListener("click", cerrarMenu);
+  cerrar.addEventListener("click", cerrarMenu);
+  // btnCarrito.addEventListener("mouseenter",mostarCarrito);
+  // btnCarrito.addEventListener("mouseout",mostarCarrito);
+  btnCarrito.addEventListener("click",mostarCarrito);
 }
 
 // Funciones
 
 function mostrarMenu() {
-    
   if (menuMobil.classList.contains("mobile")) {
     menuMobil.classList.remove("mobile");
   } else {
     menuMobil.classList.add("mobile");
-    menuMobil.style.display = "flex"
-    menuMobil.blue.restart;
-    
-    
+    menuMobil.style.display = "flex";
     
   }
 }
- function cerrarMenu() {
-   menuMobil.classList.remove("mobile");
-   menuMobil.removeAttribute("style")
+function cerrarMenu() {
+  menuMobil.classList.remove("mobile");
+  menuMobil.removeAttribute("style");
   btnMenu.classList.add("activo");
+}
+function mostarCarrito (e) {
+  
+console.log();
+const mensaje =document.querySelector('.mensaje'); 
+const perfil = document.querySelector('#perfil');
+
+ if(cardCarrito.classList.contains('activoCar')) {
+  cardCarrito.classList.remove('activoCar');
+  perfil.classList.remove('seleccionado');
+ } else{
+  cardCarrito.classList.add('activoCar');
+  perfil.classList.add('seleccionado');
+ }
 }
